9a075fa7184408bddaaf7dc34e2985d5e45efeee
Jellylama <karimbachir2001@gmail.com>
2022-04-24 18:57:42 +0200

    enhaced constructor of class ipv4Address and implemented user input

--- src/main/java/com/vlsm/App.java
+++ src/main/java/com/vlsm/App.java
@@ -1,5 +1,7 @@
 package com.vlsm;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /*System.out.println("-Enter ip address: ");
+        Scanner scanner = new Scanner(System.in);
+        String ip = scanner.next();
+        System.out.println("-Enter subnet mask: ");
+        String sm = scanner.next();
+        scanner.close();*/
+        
+        ipv4Address ipv4a = new ipv4Address("192.168.4.0", "255.255.255.0");
+        System.out.println(ipv4a.toString());
+
+        System.out.println("-Enter the number of LANs: ");
+        Scanner scanner = new Scanner(System.in);
+        int nLan = scanner.nextInt();
+        int[] hosts = new int[nLan];
+        for (int i = 0; i < nLan; i++) {
+            System.out.println("-Hosts for LAN " + (i+1) + ":");
+            hosts[i] = scanner.nextInt();
+        }
+        scanner.close();
     }
 }
--- src/main/java/com/vlsm/ipv4Address.java
+++ src/main/java/com/vlsm/ipv4Address.java
@@ -1,12 +1,55 @@
 package com.vlsm;
 
 public class ipv4Address {
-    private final int[] address;
+    private final int[] ip;
     private final int[] subnetMask;
 
-    public ipv4Address(int[] address, int[] subnetMask) {
-        this.address = address;
+    public ipv4Address(int[] ip, int[] subnetMask) {
+        this.ip = ip;
         this.subnetMask = subnetMask;
+    }
+
+    public ipv4Address(String ip, String subnetMask) {
+        String[] splittedIp = ip.split("\\.");
+        int[] intSplittedIp = {Integer.parseInt(splittedIp[0]), Integer.parseInt(splittedIp[1]),Integer.parseInt(splittedIp[2]), Integer.parseInt(splittedIp[3])};
+        this.ip = intSplittedIp;
+        
+        if (subnetMask.contains(".")) {
+            String[] splittedSm = subnetMask.split("\\.");
+            int[] intSplittedSm = {Integer.parseInt(splittedSm[0]), Integer.parseInt(splittedSm[1]), Integer.parseInt(splittedSm[2]), Integer.parseInt(splittedSm[3])};
+            this.subnetMask = intSplittedSm;
+        } else {
+            int cidr = Integer.parseInt(subnetMask);
+            String bits = "";
+            int count = 0;
+            while(cidr>0){
+                while(count != 8 && cidr>0){
+                    bits += "1";
+                    cidr--;
+                    count++;
+                }
+                count = 0;
+                if(cidr != 0)
+                    bits += ".";
+            }
+            //System.out.println(bits);
+            count = bits.split("\\.")[bits.split("\\.").length -1].length();
+            int nBits = bits.replaceAll("\\.", "").length();
+            while(nBits < 32){
+                while(count != 8){
+                    bits += "0";
+                    nBits++;
+                    count++;
+                }
+                count = 0;
+                if(nBits != 32)
+                    bits += ".";
+            }
+            String[] splittedSm = bits.split("\\.");
+            //System.out.println(bits);
+            int[] intSplittedSm = {Integer.parseInt(splittedSm[0], 2), Integer.parseInt(splittedSm[1], 2), Integer.parseInt(splittedSm[2], 2), Integer.parseInt(splittedSm[3], 2)};
+            this.subnetMask = intSplittedSm;
+        }
     }
 
     public int getCidr(){
@@ -19,6 +62,6 @@ public class ipv4Address {
     }
 
     public String toString(){
-        return this.address[0] + this.address[1] + this.address[2] + this.address[3] + " /" + this.getCidr();
+        return this.ip[0] + "." + this.ip[1] + "." + this.ip[2] + "." + this.ip[3] + " /" + this.getCidr();
     }
 }
